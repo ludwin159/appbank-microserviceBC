d08731159d5f7028eaa5e24176376745c88efd75
DevFer <devfernando23@gmail.com>
2025-02-28 16:06:43 -0500

    jkl

--- src/main/java/com/bank/appbank/service/DebitCardService.java
+++ src/main/java/com/bank/appbank/service/DebitCardService.java
@@ -1,8 +1,11 @@
 package com.bank.appbank.service;
 
 import com.bank.appbank.model.DebitCard;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface DebitCardService extends ServiceT<DebitCard, String> {
     public Mono<DebitCard> update(String idDebitCard, DebitCard debitCard);
+
+    Flux<DebitCard> findAllByClientId(String clientId);
 }
--- src/main/java/com/bank/appbank/service/impl/DebitCardServiceImp.java
+++ src/main/java/com/bank/appbank/service/impl/DebitCardServiceImp.java
@@ -1,16 +1,24 @@
 package com.bank.appbank.service.impl;
 
+import com.bank.appbank.exceptions.ResourceNotFoundException;
 import com.bank.appbank.factory.RepositoryFactory;
 import com.bank.appbank.model.DebitCard;
+import com.bank.appbank.repository.DebitCardRepository;
 import com.bank.appbank.service.DebitCardService;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.Collections;
 
 @Service
 public class DebitCardServiceImp extends ServiceGenImp<DebitCard, String> implements DebitCardService {
 
-    public DebitCardServiceImp(RepositoryFactory repositoryFactory) {
+    private final DebitCardRepository debitCardRepository;
+
+    public DebitCardServiceImp(RepositoryFactory repositoryFactory, DebitCardRepository debitCardRepository) {
         super(repositoryFactory);
+        this.debitCardRepository = debitCardRepository;
     }
 
     @Override
@@ -19,19 +27,56 @@ public class DebitCardServiceImp extends ServiceGenImp<DebitCard, String> implem
     }
 
     @Override
-    public Mono<DebitCard> create(DebitCard document) {
-        // Validaciones
-        return super.create(document);
+    public Mono<DebitCard> updateDebitCard(String id, DebitCard updatedCard) {
+        return debitCardRepository.findById(id)
+                .flatMap(existingCard -> {
+                    existingCard.setNumberCard(updatedCard.getNumberCard());
+                    existingCard.setIdClient(updatedCard.getIdClient());
+                    existingCard.setIdPrincipalAccount(updatedCard.getIdPrincipalAccount());
+                    existingCard.setBankAccounts(updatedCard.getBankAccounts());
+                    return debitCardRepository.save(existingCard);
+                })
+                .switchIfEmpty(Mono.error(new ResourceNotFoundException("Debit card not found with ID: " + id)));
+    }
+
+    @Override
+    public Mono<DebitCard> create(DebitCard newCard) {
+        return checkIfDebitCardExists(newCard.getNumberCard())
+                .flatMap(exists -> {
+                    if ("NOT_EXISTS".equals(exists)) {
+                        return debitCardRepository.save(newCard);
+                    }
+                });
+    }
+
+    private Mono<String> checkIfDebitCardExists(String cardNumber) {
+        return debitCardRepository.findById(cardNumber)
+                .map(existing -> "EXIST")
+                .defaultIfEmpty("NOT_EXISTS");
     }
 
     @Override
     public Mono<DebitCard> update(String idDebitCard, DebitCard debitCard) {
-        return getRepository().findById(idDebitCard)
-                .flatMap(debitCard1 -> {
-                    debitCard1.setNumberCard(debitCard.getNumberCard());
-                    debitCard1.setIdClient(debitCard.getIdClient());
-                    debitCard1.setIdPrincipalAccount(debitCard.getIdPrincipalAccount());
-                    return getRepository().save(debitCard1);
-                });
+        return debitCardRepository.findById(idDebitCard)
+                .flatMap(existingCard -> {
+                    existingCard.setNumberCard(debitCard.getNumberCard());
+                    existingCard.setIdClient(debitCard.getIdClient());
+                    existingCard.setIdPrincipalAccount(debitCard.getIdPrincipalAccount());
+                    existingCard.setBankAccounts(debitCard.getBankAccounts());
+                    return debitCardRepository.save(existingCard);
+                })
+                .switchIfEmpty(Mono.error(new ResourceNotFoundException("Debit card not found with ID: " + id)));
+    }
+
+    @Override
+    public Flux<DebitCard> findAllByClientId(String clientId) {
+        return debitCardRepository.findAllById(Collections.singleton(clientId));
+    }
+
+    @Override
+    public Mono<Void> deleteDebitCard(String id) {
+        return debitCardRepository.findById(id)
+                .flatMap(existingCard -> debitCardRepository.delete(existingCard))
+                .switchIfEmpty(Mono.error(new ResourceNotFoundException("Debit card not found with ID: " + id)));
     }
 }
